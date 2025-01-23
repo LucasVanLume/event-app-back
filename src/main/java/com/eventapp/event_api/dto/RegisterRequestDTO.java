5ee7e0065145f6ca795489d11db5e73d13139d1a
lvll <lucasvanluma@hotmail.com>
2025-01-23 15:54:04 -0300

    add DTO's for login and registration

---
+++ src/main/java/com/eventapp/event_api/dto/RegisterRequestDTO.java
@@ -0,0 +1,13 @@
+package com.eventapp.event_api.dto;
+
+public record RegisterRequestDTO (String name, String email, String password) {
+    /*private String name;
+    private String email;
+    private String password;
+
+    public RegisterRequestDTO(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }*/
+}
