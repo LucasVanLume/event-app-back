938dc9a7ba464c068dbe9cbbb403e8e3fe269274
lvll <lucasvanlume@hotmail.com>
2025-02-07 08:43:00 -0300

    feat: add userId as login and register return

--- src/main/java/com/eventapp/event_api/controllers/auth/AuthController.java
+++ src/main/java/com/eventapp/event_api/controllers/auth/AuthController.java
@@ -36,7 +36,7 @@ public class AuthController {
         User user = this.repository.findByEmail(body.email()).orElseThrow(() -> new RuntimeException("User not found"));
         if(passwordEncoder.matches(body.password(), user.getPassword())) {
             String token = this.tokenService.generateToken(user);
-            return ResponseEntity.ok(new LoginResponseDTO(user.getName(), token));
+            return ResponseEntity.ok(new LoginResponseDTO(user.getName(), token, user.getId()));
         }
         return ResponseEntity.badRequest().build();
     }
@@ -54,7 +54,7 @@ public class AuthController {
             this.repository.save(newUser);
 
             String token = this.tokenService.generateToken(newUser);
-            return ResponseEntity.ok(new LoginResponseDTO(newUser.getName(), token));
+            return ResponseEntity.ok(new LoginResponseDTO(newUser.getName(), token, newUser.getId()));
         }
         return ResponseEntity.badRequest().build();
     }
